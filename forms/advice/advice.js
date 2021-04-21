6830a86b3099f51bc8840ceb662debebd5fc004a
kes97391 <79420804+kes97391@users.noreply.github.com>
2021-04-21 10:08:14 -0500

    more changes

---
+++ forms/advice/advice.js
@@ -0,0 +1,66 @@
+/*
+
+// 1. *** use your own url copied from Postman ****
+let requestURL = "https://api.spoonacular.com/recipes/complexSearch?query=pasta&instructionsRequired=True&addRecipeInformation=True&apiKey=9756a88fa0a145fb93499bc42cf60e7b"
+
+function onXHRLoad() {
+    let message = ""
+    
+    // 'this' is another name for the object returned from the API call
+    let apiData = JSON.parse(this.responseText)
+    
+    for (i = 0; i <= apiData.results.length - 1; i++) {
+        console.log(`${apiData.results[i].title}`)
+        message = message + apiData.results[i].title + "\n"
+    }
+    
+    // 2. *** put your textarea control name here ****
+    Textarea1.value = message
+    
+    // if want to add to database call a function here that does that
+    // addToDatabase()
+}
+
+function callAPI(URL) {
+    var xhttp = new XMLHttpRequest();
+    
+    // if you need cors (you'll get a cors error if you don't have it and you need it)
+    // use this code to add the cors code to your url 
+    //xhttp.open('GET', 'https://cors.bridged.cc/' + requestURL)
+    
+    // if you DON'T need cors use this code:
+    xhttp.open('GET',URL)
+    
+    /* Headers */
+    // if you need to set the returned data type, use this line of code: 
+    //xhttp.setRequestHeader('Content-Type', 'application/json')
+    
+    // if you need authorization token (stored in myToken) use this line of code: 
+    // xhttp.setRequestHeader('Authorization', 'Bearer ' + myToken)
+    
+    // if you need a key and it's not in the url use code in one of the following
+    // examples (think of headers as parameters)
+    // or just use the Postman url which has all the parameters already added like I did here. 
+    
+    
+    // Here are headers you might need: 
+    /*
+    xhttp.setRequestHeader('key','AIzaSyCE-pjULPU_Gp5Qf0qL39tVsdJBX55J0cY')
+    xhttp.setRequestHeader('location','41.276900,-95.942310')
+    xhttp.setRequestHeader('rankby','distance')
+    xhttp.setRequestHeader('type','restaurant')
+    
+
+    // make the API request
+    xhttp.addEventListener('load', onXHRLoad)
+    xhttp.send()
+}
+
+// 3. *** add a new button onclick event and put the callAPI code into it ***
+btnClickMe.onclick=function(){
+    // call the code that will make the API call, then process what comes back
+    callAPI(requestURL)
+}
+
+//console.log(results.results[0].glutenFree)
+*/
--- forms/recipeConfirmation/recipeConfirmation.js
+++ forms/recipeConfirmation/recipeConfirmation.js
@@ -123,26 +123,7 @@ btnAddIngredient.onclick=function(){
         lblMessage3.textContent = "Error: " + req.status
 }
 
-btnAddIngredient.onclick=function(s){
-  if (typeof(s) == "object")
-    return 
-  else 
-    drpSelection.value = s
-    let ingredients = drpSelection.value 
-  
-query = "SELECT * FROM lyst_ingredients t INNER JOIN `lyst` l ON t.lyst_id = l.lyst_id INNER JOIN `ingredients` i ON i.ingredient_id = t.ingredient_id WHERE lyst_name = '" + ingredients + "'"
-   req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + "375groupa3" + "&query=" + query)
-  if (req.status == 200) { 
-    console.log(`req.responseText is a JSON string that looks like this: ${req.responseText}`)
-  results = JSON.parse(req.responseText)
-    console.log(`The parsed JSON string is converted to a JS object (an array of arrays): ${results} where results[0] is ${results[0]}, the first array in the JS results object.`)
-  if (results.length == 0)
-    lblTest.value = "There are no ingredients in the database"
-  else {
-  let message = ""
-  for (i = 0; i < results.length; i++)
-    rdoConfirmation.addItem(message + results[i][8] + "\n")
-    }
-  } else 
-    lblTest.value = "Error Code:" + req.status
+btnAddIngredient.onclick=function(){
+  let addIngredient = inptAddition.value
+  rdoConfirmation.addItem(addIngredient)
 }

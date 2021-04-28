ef02023edf4235338873471f83f8b8fa3d3e94f7
kes97391 <79420804+kes97391@users.noreply.github.com>
2021-04-28 13:15:14 -0500

    more

--- forms/calculator/calculator.js
+++ forms/calculator/calculator.js
@@ -2,17 +2,9 @@
 btnBegin.onclick=function(){
   int1 = parseInt(inptValueOne.value)
   int2 = parseInt(inptValueTwo.value)
-  lblAmount.value = int1 + int2
+  int3 = parseInt(inptValueThree.value)
+  lblAmount.value = int1 + int2 + int3
 }
-
-/*
-btnMult.onclick=function(){
-  int1 = parseInt(inptValueOne.value)
-  int2 = parseInt(inptValueTwo.value)
-  lblDisplay.value = int1 * int2
-}
-*/
-
 
 btnClear.onclick=function(){
   lblAmount.value = ""
@@ -63,33 +55,12 @@ query = "SELECT * FROM lyst_ingredients t INNER JOIN `lyst` l ON t.lyst_id = l.l
     lblAmount.value = "Error Code:" + req.status
 }
 
-/*
-drpCalculator3.onclick=function(s){
-  if (typeof(s) == "object")
-    return 
-  else 
-    drpCalculator2.value = s
-    let ingredients = drpCalculator3.value 
-  
-query = "SELECT * FROM lyst_ingredients t INNER JOIN `lyst` l ON t.lyst_id = l.lyst_id INNER JOIN `ingredients` i ON i.ingredient_id = t.ingredient_id WHERE lyst_name = '" + ingredients + "' LIMIT 1"
-   req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + "375groupa3" + "&query=" + query)
-  if (req.status == 200) { 
-    console.log(`req.responseText is a JSON string that looks like this: ${req.responseText}`)
-  results = JSON.parse(req.responseText)
-    console.log(`The parsed JSON string is converted to a JS object (an array of arrays): ${results} where results[0] is ${results[0]}, the first array in the JS results object.`)
-  if (results.length == 0)
-    lblAmount.value = "There are no ingredients in the database"
-  else {
-  let message = ""
-  for (i = 0; i < results.length; i++)
-    lstgCalculator3.addItem(message + results[i][7] + "\n")
-    }
-  } else 
-    lblAmount.value = "Error Code:" + req.status
+
+
+btnHomeAwayFromHome.onclick=function(){
+  ChangeForm(afterLogin)
 }
 
-btnBegin.onclick=function(){
-  int1 = parseInt(lstgCalculator.value)
-  lblAmount.value = int1 + 
+btnHomeOnThaRange.onclick=function(){
+  ChangeForm(recipeConfirmation)
 }
-*/

088cad618b35de8212bbfb82b79422f989783c50
Duncan-Kyhl <Duncan-Kyhl@users.noreply.github.com>
2021-05-02 18:20:16 -0500

    map

--- forms/map/map.js
+++ forms/map/map.js
@@ -14,3 +14,30 @@ btnWohlners.onclick=function(){
 btnHomebound.onclick=function(){
   ChangeForm(afterLogin)
 }
+
+// Create two div elements named panel and map
+let panelDiv = document.createElement('div')
+panelDiv.setAttribute('id', 'panel')
+
+let mapDiv = document.createElement('div')
+mapDiv.setAttribute('id', 'map')
+
+// Add the panel and map divs to the <body>
+document.body.appendChild(panelDiv)
+document.body.appendChild(mapDiv)
+
+// Create script element for main JS map functionality
+var mapScript = document.createElement("script");
+// Add script content
+mapScript.type = "text/javascript"
+mapScript.src = "main.js"
+// Append
+document.body.appendChild(mapScript);
+
+// Create script element for API connection
+var apiScript = document.createElement("script");
+// Add script content
+apiScript.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAUYQCfTMugPMx1OT2BiGQnppgwKIWsI9g&libraries=places&callback=initMap">
+// apiScript.async = true
+// Append
+document.body.appendChild(apiScript);
